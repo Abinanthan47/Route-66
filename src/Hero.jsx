364b132228abb47d426c68bd9fffd85453ce9c8a
Abinanthan <143276733+Abinanthan47@users.noreply.github.com>
2025-01-01 23:45:36 +0530

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,23 +1,40 @@
-import { ArrowRightCircleIcon } from 'lucide-react'
-import { Link } from 'react-router-dom'
-import { Button } from './components/ui/button'
-import Search from './Search'
+import { ArrowRightCircleIcon } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import { Button } from './components/ui/button';
+import Search from './Search';
 
 function Hero() {
   return (
-    <div className='md:px-5 '>
-      <div className='bg-heromob border-black border md:bg-hero  md:rounded-2xl w-full h-screen flex flex-col md:justify-center items-center'>
-        <div className='flex flex-col  mt-48 md:mt-20 md:mix-blend-exclusion md:text-white text-center'>
-          <h3 className='text-2xl font-medium md:text-3xl'>Find your Dream</h3>
-          <h2 className='text-5xl font-bold md:text-7xl'>Vintage Cars</h2>
-          <Link to={'/collection'} className=''>
-            <Button  className=' mt-8 px-3 border border-black rounded-full text-md md:hidden'>Collections <ArrowRightCircleIcon /> </Button>
+    <div className="md:px-5">
+      <div
+        className="w-full h-screen flex flex-col md:justify-center items-center border-black border md:rounded-2xl"
+        style={{
+          backgroundImage: `url('/bg-hero-mob.jpg')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      >
+        <div
+          className="hidden md:block absolute inset-0"
+          style={{
+            backgroundImage: `url('/bg-hero.jpg')`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        ></div>
+        <div className="flex flex-col mt-48 md:mt-20 text-center md:mix-blend-exclusion md:text-white">
+          <h3 className="text-2xl font-medium md:text-3xl">Find your Dream</h3>
+          <h2 className="text-5xl font-bold md:text-7xl">Vintage Cars</h2>
+          <Link to={'/collection'}>
+            <Button className="mt-8 px-3 border border-black rounded-full text-md md:hidden">
+              Collections <ArrowRightCircleIcon />
+            </Button>
           </Link>
         </div>
         <Search />
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
