6451ae799863bd180a6b7a76a55c5d1aab00fedf
Abinanthan <143276733+Abinanthan47@users.noreply.github.com>
2025-01-01 23:42:05 +0530

    Update Hero.jsx

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -6,7 +6,7 @@ import Search from './Search'
 function Hero() {
   return (
     <div className='md:px-5 '>
-      <div className='bg-hero-mob border-black border md:bg-hero  md:rounded-2xl w-full h-screen flex flex-col md:justify-center items-center'>
+      <div className='bg-heromob border-black border md:bg-hero  md:rounded-2xl w-full h-screen flex flex-col md:justify-center items-center'>
         <div className='flex flex-col  mt-48 md:mt-20 md:mix-blend-exclusion md:text-white text-center'>
           <h3 className='text-2xl font-medium md:text-3xl'>Find your Dream</h3>
           <h2 className='text-5xl font-bold md:text-7xl'>Vintage Cars</h2>
